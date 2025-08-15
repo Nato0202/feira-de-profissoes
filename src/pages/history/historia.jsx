e7817e5fbb66df4c58fc98a935e953924f9d2857
Renan <josenilton1630@gmail.com>
2025-08-15 08:46:54 -0300

    Update historia.jsx

---
+++ src/pages/history/historia.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './historia.scss'
+
+function history() {
+
+  return (
+    <>
+      <nav>
+        <img src="" alt="logo" />
+        <div className="searchBar">
+          <input type="Pesquisa"/>
+        </div>
+        <div className="menu">
+          
+        </div>
+      </nav>
+      <main>
+        
+      </main>
+      <footer>
+        
+      </footer>
+    </>
+  )
+}
+
+export default history;
